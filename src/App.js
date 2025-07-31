a5fc6019ba2f259b5f9fb9ac937d03c975eb4326
rafiq <mohamadrafi547@gmail.com>
2025-07-31 15:28:45 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
 // import logo from './logo.svg';
 import './App.css';
 
+
 function App() {
   return (
     <div className="App">
-     
-        {/* <img src={logo} className="App-logo" alt="logo" /> */}
-       <h1>Welcome to Git Hub</h1>
-       <h2>App page chnaged</h2>
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
